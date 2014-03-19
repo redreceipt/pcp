9b3409eda9fb8e20ec0339d1a548447069b465a3
Michael Neeley <micneeley14@gmail.com>
2014-03-19 16:00:46 -0400

    added talk and shutdown functionality

--- computers.py
+++ computers.py
@@ -1,31 +1,50 @@
 #!/bin/sh python
-#
-# requires the sshpass utility
 
 import os, time
 import paramiko, base64
+import ConfigParser
 
-def openConnection(ip, hostkey, remoteUsername, remotePassword):
-	key = paramiko.RSAKey(data = base64.decodestring(hostkey))
-	client = paramiko.SSHClient()
-	client = get_host_keys().add(ip, "ssh-rsa", key)
-	client.connect(ip, username = remoteUsername, password = remotePassword)
-	return client
+# computer class
+class _Computer:
+	
+	def __init__(self, name):
+		
+		config = ConfigParser.ConfigParser()
+		config.read("hardware.cfg")
+		
+		self.ip = config.get(name, "ip")
+		self.key = config.get(name, "rsaKey")
+		self.username = config.get(name, "username")
+		self.password = config.get(name, "password")
+		self.client = None
+
+	def openConnection(self):
+		key = paramiko.RSAKey(data = base64.decodestring(hostkey))
+		self.client = paramiko.SSHClient()
+		self.client.get_host_keys().add(ip, "ssh-rsa", key)
+		self.client.connect(self.ip, username = self.username, password = self.password)
+	
+	def closeConnection(self):
+		self.client.close()
+	
+	def talk(self, message):
+		
+		# uses the say application
+		self.client.exec_command("say" + message)
+
+	def shutdown(self):
+		
+		# sudo shutdown and type in the password
+		self.client.exec_command("sudo shutdown now")
+		time.sleep(1)
+		self.client.exec_command(self.password)
 
 def main():
 	
-	computers = {
-		"play1": {"ip": "10.5.30.11", "key": "AAAAB3NzaC1yc2EAAAADAQABAAABAQDJ2KP+WJW5Uzakgk03dIW34AUoLOXaQbGed7aDj6NXeyVCgc7iK7BySeRc9RgU1rccSZm5Q8m7I5YA+xOpv5f00qVsrVZSlMMfIL955YUFd3yKTVdf15RUE4Dz1hyv1LHUZ4CnN19wAhQ+FZdzcWwYhLczwvZ2OMZbTi2PAiqyHlc55n+iKGX1ovQf7qEoA0M1qrMaDaaTYEq1O5yvKgi3s3uZGZK3Bnthy28P9CexXWRrbLGXkDBH52UFyABbbEqUZ3672CbTpByE+2V3I3B2WjgPinMH8Dl5zLdYy7jwxVaOgWTO/Q8IsvNyGOIbc6+ct2RNt9Uny+MwRAhju7Rl"},
-		"pvp": "10.5.30.8",
-		"cg": "10.5.30.7",
-		"teleprompt": "10.5.30.9"
-	}
-
-	# connect to play 1 and shutdown
-	computer = computers["play1"]
-	client = openConnection(computer["ip"], computer["key"], "presentation", "Pr3s3ntation")
-	###client.exec_command("say hello world")
-	client.close()
+	# play1
+	play1 = _Computer("play1")
+	play1.talk("shutting down now")
+	#play1.shutdown()
 	
 	# shut down teleprompt
 	# shut down cg
